ec1ca90048ae5c46dde8d0a0df79109ae604c63a
ajitsingh <jeetsingh.ajit@gmail.com>
2015-04-11 13:05:41 +0530

    [Ajit] refactoring callbacks with promises

--- nodejs/src/booking_engine.js
+++ nodejs/src/booking_engine.js
@@ -1,24 +1,55 @@
 var pg = require('pg');
+var Promise = require('bluebird');
 var Transaction = require('pg-transaction');
-var connection = "postgres://postgres:@localhost/booking_engine";
+var connection = "postgres://postgres:@localhost/tickets";
 
 var seats = ["A0", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9",
     "B0", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9",
     "C0", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9"];
 
-var getAvailableTickets = function (sessionId, callback) {
-    var client = new pg.Client(connection);
-    client.connect();
-    client.query('SELECT seat_name from orders_seats where session_id = $1', [sessionId], function (err, result) {
-        var bookedSeats = result.rows.map(function (row) {
-            return row.seat_name;
-        });
+var getNextOrderId = function(){
+    return new Promise(function(resolve, reject){
+        var client = new pg.Client(connection);
+        client.connect();
 
-        var availableSeats = seats.filter(function (i) {
-            return bookedSeats.indexOf(i) == -1
+        client.query("select nextval('orders_id_seq'::regclass)", function (err, result) {
+            var nextId = result.rows[0].nextval;
+            client.end();
+            resolve(nextId);
         });
+    });
+};
 
-        callback(availableSeats);
+var getNextOrderSeatsId = function(){
+    return new Promise(function(resolve, reject) {
+        var client = new pg.Client(connection);
+        client.connect();
+
+        client.query("select nextval('order_seats_id_seq'::regclass)", function (err, result) {
+            var nextId = result.rows[0].nextval;
+            client.end();
+            resolve(nextId);
+        });
+    });
+};
+
+var getAvailableTickets = function (sessionId) {
+    return new Promise(function(resolve, reject) {
+        var client = new pg.Client(connection);
+        client.connect();
+
+        client.query('SELECT booked_seat from order_seats where session_id = $1', [sessionId], function (err, result) {
+            var bookedSeats = result.rows.map(function (row) {
+                return row.booked_seat;
+            });
+
+            var availableSeats = seats.filter(function (i) {
+                return bookedSeats.indexOf(i) == -1
+            });
+
+            client.end();
+            resolve(availableSeats);
+        });
     });
 };
 
@@ -27,8 +58,30 @@ var throwErr = function(err){
     if(err) throw err;
 };
 
+var persistOrder = function(tx){
+  return new Promise(function(resolve, reject){
+      getNextOrderId().then(function(orderId){
+          tx.query('insert into orders(id) values($1)', [orderId]);
+          resolve(orderId);
+      });
+  });
+};
+
+var persistSeats = function(tx, seats, orderId, sessionId){
+    return new Promise(function(resolve, reject){
+        var i = 0;
+        seats.forEach(function (seat) {
+            getNextOrderSeatsId().then(function(orderSeatId){
+                tx.query('insert into order_seats(id, order_id, session_id, booked_seat) values($1, $2, $3, $4)', [orderSeatId, orderId, sessionId, seat]);
+                i += 1;
+                if(i == seats.length) resolve();
+            });
+        });
+    });
+};
+
 exports.getAvailableTickets = function (req, res) {
-    getAvailableTickets(req.param('id'), function (availableTickets) {
+    getAvailableTickets(req.param('id')).then(function (availableTickets) {
         res.send(availableTickets);
     });
 };
@@ -42,7 +95,7 @@ exports.bookTickets = function (req, res) {
     var tx = new Transaction(client);
     tx.on('error', throwErr);
 
-    getAvailableTickets(sessionId, function (availableSeats) {
+    getAvailableTickets(sessionId).then(function (availableSeats) {
         var ticketsNotAvailable = seats.filter(function (seat) {
             return availableSeats.indexOf(seat) == -1;
         }).length;
@@ -52,14 +105,13 @@ exports.bookTickets = function (req, res) {
 
         } else {
             tx.begin();
-            tx.query('insert into orders(id) values($1)', [req.id]);
-
-            seats.forEach(function (seat) {
-                tx.query('insert into orders_seats(id, session_id, seat_name) values($1, $2, $3)', [req.id, sessionId, seat]);
-            });
-            tx.commit(function () {
-                client.end();
-                res.send('OK');
+            persistOrder(tx).then(function(orderId){
+                persistSeats(tx, seats, orderId, sessionId).then(function(){
+                    tx.commit(function(){
+                        client.end();
+                        res.send('OK');
+                    });
+                });
             });
         }
     });
