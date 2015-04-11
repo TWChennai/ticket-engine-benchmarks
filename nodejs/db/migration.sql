5a58b911a013a9f4f0412311b6ecda71d872f1d7
Selva <k.n.selvakumar@gmail.com>
2015-04-11 07:51:10 +0530

    Schema - made it generic as per our discussion

--- nodejs/db/migration.sql
+++ nodejs/db/migration.sql
@@ -2,20 +2,60 @@ DROP TABLE IF EXISTS sessions;
 DROP TABLE IF EXISTS orders;
 DROP TABLE IF EXISTS orders_seats;
 
-create table sessions(id SERIAL, name varchar);
-create table orders(id VARCHAR UNIQUE, created_at TIMESTAMP DEFAULT now());
-create table orders_seats(id VARCHAR, session_id INTEGER, seat_name VARCHAR);
+CREATE TABLE sessions (
+    id integer NOT NULL,
+    name text
+);
 
-insert into sessions(name) values('FAST AND FURIOUS 7');
-insert into sessions(name) values('KINGS MAN');
-insert into sessions(name) values('PRESTIGE');
-insert into sessions(name) values('HOBBIT');
-insert into sessions(name) values('CITY OF EMBER');
-insert into sessions(name) values('THE MECHANIC');
-insert into sessions(name) values('THE MARIN');
-insert into sessions(name) values('EYES WIDE SHUT');
-insert into sessions(name) values('BATMAN');
-insert into sessions(name) values('SUPERMAN');
-insert into sessions(name) values('TERMINATOR');
-insert into sessions(name) values('TERMINATOR');
+CREATE SEQUENCE sessions_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
+CREATE TABLE orders (
+    created_at TIMESTAMP DEFAULT now(),
+    id integer NOT NULL
+);
+
+CREATE SEQUENCE orders_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE order_seats (
+    id integer NOT NULL,
+    booked_seat text,
+    session_id integer NOT NULL,
+    order_id integer NOT NULL
+);
+
+CREATE SEQUENCE order_seats_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE SEQUENCE order_seats_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+insert into sessions(id, name) values(1, 'FAST AND FURIOUS 7');
+insert into sessions(id, name) values(2, 'KINGS MAN');
+insert into sessions(id, name) values(3, 'PRESTIGE');
+insert into sessions(id, name) values(4, 'HOBBIT');
+insert into sessions(id, name) values(5, 'CITY OF EMBER');
+insert into sessions(id, name) values(6, 'THE MECHANIC');
+insert into sessions(id, name) values(7, 'THE MARIN');
+insert into sessions(id, name) values(8, 'EYES WIDE SHUT');
+insert into sessions(id, name) values(9, 'BATMAN');
+insert into sessions(id, name) values(10, 'SUPERMAN');
+insert into sessions(id, name) values(11, 'TERMINATOR');
